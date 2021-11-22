4e4d3eb6479e2ac1296b7faf1c3fa17f454c63e1
Valentin <valentin.d77@free.fr>
2021-11-22 18:53:34 +0100

    ajout de la fonction tangente

---
+++ src/Controllers/Operators/Functions/Tangent.java
@@ -0,0 +1,38 @@
+package Controllers.Operators.Functions;
+
+import Controllers.Operands.Number;
+import Controllers.Operands.Operand;
+
+/**
+ * Représente la fonction tangente.
+ *
+ * @author Valentin DOREAU
+ * @see <a href="https://en.wikipedia.org/wiki/Trigonometric_functions">Trigonometric functions</a>
+ */
+public class Tangent extends Function {
+    /**
+     * {@inheritDoc}
+     *
+     * @throws ArithmeticException
+     */
+    @Override
+    public Number calculate(final Operand A, final Operand B, final double x) throws ArithmeticException {
+        final double a = A.calculate(x).toDouble();
+
+        // tangente n'est pas définie quand
+        // x = PI/2 + k*PI
+        if (((x - Math.PI) / 2 / Math.PI) % 1 == 0) {
+            throw new ArithmeticException("tan n'est pas définie sur PI/2 + k*PI");
+        }
+
+        return new Number(Math.tan(a));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String symbol() {
+        return "tan";
+    }
+}
