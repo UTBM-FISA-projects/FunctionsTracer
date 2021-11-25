3b79e24c9d07db6a1929f28b1f6cf428cbee9af4
Valentin <valentin.d77@free.fr>
2021-11-25 20:37:34 +0100

    correction du parser pour les expression avec seulement un nombre

--- src/Controllers/Parser/Parser.java
+++ src/Controllers/Parser/Parser.java
@@ -242,6 +242,10 @@ public class Parser {
             pushOperator();
         }
 
+        if (stack.peek().getClass() == Number.class) {
+            return new Expression(stack.pop(), null, null, false);
+        }
+
         return (Expression) stack.pop();
     }
 }
