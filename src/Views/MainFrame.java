1ae7030fb0abe06af0fdf842c4b2233924eeb010
Kilian <kilian.goetz@yahoo.fr>
2021-11-25 10:51:14 +0100

    Boutons
    Ajout de bouton et de fonctionalité (Couleur de la courbe, supprimer et exporter sous forme de table

--- src/Views/MainFrame.java
+++ src/Views/MainFrame.java
@@ -5,7 +5,7 @@ import Views.MenuBar.MenuBar;
 import javax.swing.*;
 import java.awt.*;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame {
 
     private final Image img;
     private final JPanel mainPanel;
@@ -17,7 +17,7 @@ public class MainFrame extends JFrame{
         img = new ImageIcon("resources/logoNoir.png").getImage();
         setIconImage(img);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(640,360));
+        setMinimumSize(new Dimension(640, 360));
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         ExpressionList expressionList = new ExpressionList();
 
@@ -26,13 +26,15 @@ public class MainFrame extends JFrame{
                 ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
         );
+
         scrollPane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         scrollPane.setBorder(null);
-        //scrollPane.setPreferredSize(this.getSize()); //TODO redimensionner JScrollPane
+        scrollPane.setPreferredSize(this.getSize());
         mainPanel = new JPanel();
         mainPanel.add(scrollPane);
         mainPanel.add(new Canvas());
-        this.setContentPane(mainPanel);;
+        this.setContentPane(mainPanel);
+
         setJMenuBar(new MenuBar(expressionList));
         setVisible(true);
 
